d4717327b61984f9873c17a9d67a54f4ee71652e
Marc-Antoine ARNAUD <arnaud.marcantoine@gmail.com>
2013-04-09 19:01:25 +0200

    adding setup.py for uploading package on PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='MarkdownWriter',
+	version='1.0',
+	description='Markdown Writer',
+	author='Valentin NOEL, Marc-Antoine ARNAUD',
+	author_email='valent.noel@gmail.com',
+	url='https://github.com/mikrosimage/python-markdown-writer',
+	packages=['markdownwriter'],
+    )
