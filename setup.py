ea5f3bde2ddd15018af70bd5ea4822bbb71ea5e5
Marc-Antoine ARNAUD <arnaud.marcantoine@gmail.com>
2013-04-23 14:57:56 +0200

    adding markdown for pandoc export package

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import setup
 
 setup(
 	name='MarkdownWriter',
 	version='1.0',
-	description='Markdown Writer',
+	description='Markdown Writer API',
 	author='Valentin NOEL, Marc-Antoine ARNAUD',
 	author_email='valent.noel@gmail.com',
 	url='https://github.com/mikrosimage/python-markdown-writer',
 	packages=['markdownwriter'],
     )
+
+setup(
+        name='PandocMarkdownWriter',
+        version='1.0',
+        description='Pandoc Markdown Writer API',
+        author='Valentin NOEL, Marc-Antoine ARNAUD',
+        author_email='valent.noel@gmail.com',
+        url='https://github.com/mikrosimage/python-markdown-writer',
+        packages=['pandocmarkdownwriter'],
+	install_requires = ['markdownwriter>=1.0'],
+    )
+
