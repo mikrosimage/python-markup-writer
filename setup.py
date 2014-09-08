1c7ccbfec929762b169de6309d2d7d6b979a491a
Valentin Noel <van@mikrosimage.eu>
2014-09-08 18:13:12 +0200

    Packaging: change package name to MarkupWriter

--- setup.py
+++ setup.py
@@ -10,10 +10,14 @@ toLatex_path = os.path.join(dir_name, 'pythonToLatex')
 toMarkdown_path = os.path.join(dir_name, 'pythonToMarkdown')
 
 setup(
-    name='MarkdownWriter',
-    version='1.1',
-    description='Markdown Writer API',
-    keywords='Markdown',
+    name='MarkupWriter',
+    version='1.0',
+    description='Python API for writing Markdown, HTML and Latex documents',
+    keywords=[
+        'Markdown',
+        'HTML',
+        'Latex',
+    ],
     author='Valentin NOEL, Marc-Antoine ARNAUD',
     author_email='valent.noel@gmail.com',
     url='https://github.com/mikrosimage/python-markup-writer',
@@ -28,8 +32,7 @@ setup(
         'htmlwriter': os.path.join(toHtml_path, 'htmlwriter'),
         'latexwriter': os.path.join(toLatex_path, 'latexwriter'),
         'markdownwriter': os.path.join(toMarkdown_path, 'markdownwriter'),
-        'pandocmarkdownwriter': os.path.join(toMarkdown_path,
-                                             'pandocmarkdownwriter'),
+        'pandocmarkdownwriter': os.path.join(toMarkdown_path, 'pandocmarkdownwriter'),
     },
     tests_require=['unittest2'],
     test_suite='tests',
